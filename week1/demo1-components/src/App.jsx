b9fbe580e6332b27dfc9a60336728558ddd9f71a
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2023-08-31 20:15:11 -0500

    Update App.jsx
    adding components and props

--- week1/demo1-components/src/App.jsx
+++ week1/demo1-components/src/App.jsx
@@ -6,8 +6,8 @@ const App = () => {
     <div className="App">
       <h1>Welcome to Web 102 🎉</h1>
       <h3>Write a post about what you are excited to learn about!</h3>
-      <Post />
-      <Post />
+      <Post name="Marquees Brownlee" description="I want to learn how to build cleaner user interfaces." numLikes="100"/>
+      <Post name="Justine Ezarik" description="I want to become a JavaScript Wizard" numLikes="500"/>
     </div>
   )
 }
